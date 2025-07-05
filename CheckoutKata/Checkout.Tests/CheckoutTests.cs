110bcbb1773d4fc08e3a4f19462a27647a608f31
Surinder1206 <surinder.singh1206@gmail.com>
2025-07-05 22:08:50 +0100

    test: add failing test for null pricing rules in Checkout constructor
    - Add project reference from test project to main Checkout project

---
+++ CheckoutKata/Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Checkout.Tests;
+
+internal class CheckoutTests
+{
+    [Test]
+    public void Constructor_should_throw_argument_null_exception_when_pricing_rules_are_null()
+    {
+        // Arrange & Act
+        Action act = () => new Checkout(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>()
+            .WithParameterName("pricingRules");
+    }
+}
+
