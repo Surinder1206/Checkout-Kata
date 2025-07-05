94b987b2f70f78b64797d10c72cf1906409d7e8e
Surinder1206 <surinder.singh1206@gmail.com>
2025-07-06 00:47:22 +0100

    refactor: move duplicated pricing rules to test setup method
    - Rename scanItem to scannedItems to fix build errors

--- CheckoutKata/Checkout.Tests/CheckoutTests.cs
+++ CheckoutKata/Checkout.Tests/CheckoutTests.cs
@@ -5,6 +5,20 @@ namespace Checkout.Tests;
 
 internal class CheckoutTests
 {
+    private Dictionary<string, int> _pricingRules;
+
+    [SetUp]
+    public void Initialize()
+    {
+        _pricingRules = new Dictionary<string, int>
+        {
+            { "A", 50 },
+            { "B", 30 },
+            { "C", 20 },
+            { "D", 15 }
+        };
+    }
+
     [Test]
     public void Constructor_should_throw_argument_null_exception_when_pricing_rules_are_null()
     {
@@ -22,14 +36,7 @@ internal class CheckoutTests
     public void Scan_should_throw_argument_exception_when_SKU_is_null_or_empty_or_whitespace(string sku)
     {
         // Arrange
-        var pricingRules = new Dictionary<string, int>
-        {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
-        var checkout = new Checkout(pricingRules);
+        var checkout = new Checkout(_pricingRules);
 
         // Act
         Action act = () => checkout.Scan(sku);
@@ -44,16 +51,8 @@ internal class CheckoutTests
     public void Scan_should_throw_argument_exception_when_unknown_item_is_scanned()
     {
         // Arrange
-        var pricingRules = new Dictionary<string, int>
-        {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
-
         var sku = "invalid";
-        var checkout = new Checkout(pricingRules);
+        var checkout = new Checkout(_pricingRules);
 
         // Act
         Action act = () => checkout.Scan(sku);
@@ -67,15 +66,7 @@ internal class CheckoutTests
     public void GetTotalPrice_should_return_zero_when_no_items_scanned()
     {
         // Arrange
-        var pricingRules = new Dictionary<string, int>
-        {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
-
-        Checkout checkout = new(pricingRules);
+        Checkout checkout = new(_pricingRules);
 
         // Act
         var totalPrice = checkout.GetTotalPrice();
@@ -91,15 +82,7 @@ internal class CheckoutTests
     public void GetTotalPrice_should_return_total_price_when_single_item_is_scanned(string scannedItem, int expectedPrice)
     {
         // Arrange
-        var pricingRules = new Dictionary<string, int>
-        {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
-
-        Checkout checkout = new(pricingRules);
+        Checkout checkout = new(_pricingRules);
         checkout.Scan(scannedItem);
 
         // Act
@@ -118,15 +101,8 @@ internal class CheckoutTests
     public void GetTotalPrice_should_return_correct_total_for_multiple_scanned_items(string[] scannedItems, int expectedTotal)
     {
         // Arrange
-        var pricingRules = new Dictionary<string, int>
-        {
-            { "A", 50 },
-            { "B", 30 },
-            { "C", 20 },
-            { "D", 15 }
-        };
-        var checkout = new Checkout(pricingRules);
-        foreach (var item in scanItems)
+        var checkout = new Checkout(_pricingRules);
+        foreach (var item in scannedItems)
         {
             checkout.Scan(item);
         }
