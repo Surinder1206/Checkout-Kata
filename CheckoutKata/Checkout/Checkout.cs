efb55903b8d9c02e907296598a23ef964a1d4087
Surinder1206 <surinder.singh1206@gmail.com>
2025-07-05 22:11:32 +0100

    feat: throw ArgumentNullException when pricing rules are null injected in Checkout constructor

---
+++ CheckoutKata/Checkout/Checkout.cs
@@ -0,0 +1,17 @@
+﻿namespace Checkout;
+
+public class Checkout(Dictionary<string, int> pricingRules) : ICheckout
+{
+    private readonly Dictionary<string, int> _pricingRules = pricingRules
+        ?? throw new ArgumentNullException(nameof(pricingRules), "Pricing rules must not be null");
+
+    public void Scan(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+}
